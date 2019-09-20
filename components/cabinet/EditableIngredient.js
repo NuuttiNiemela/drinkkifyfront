7031d49859f7e06c9c5443fd3b49c375fd2332d4
NuuttiNiemela <nuutti.niemela@hotmail.com>
2019-09-20 13:14:34 +0300

    paranneltu settingsejä ja omat aineet voi muuttaa

--- components/cabinet/EditableIngredient.js
+++ components/cabinet/EditableIngredient.js
@@ -1,23 +1,45 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View,} from "react-native";
+import {StyleSheet, Text, View, TextInput} from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 class EditableIngredient extends Component {
+    state={ newName: '', textVisible: true, inputVisible: false, }
 
-    edit = () => {
-        this.props.edit(this.props.ingredient.id)
+    edit = async () => {
+        await this.props.edit(this.props.ingredient.id, this.state.newName)
+    }
+
+    changeName = () => {
+        if(this.state.textVisible) {
+            this.setState({textVisible: false, inputVisible: true})
+        } else {
+            if(this.state.newName.trim() !== '') {
+                this.edit()
+                    .then(() => alert('Ingredients name changed'))
+                    .then(() => this.setState({textVisible: true, inputVisible: false, newName: ''}))
+            } else {
+            this.setState({textVisible: true, inputVisible: false, newName: ''})
+            }
+        }
     }
 
     render() {
         return (
             <View>
                 <View style={{flexDirection: 'row', justifyContent: 'space-between', marginRight: '5%',}}>
-                    <Text style={styles.viinaStyle}>{this.props.ingredient.ingredient_name} </Text>
+                    { this.state.textVisible &&
+                        <Text style={styles.viinaStyle}>{this.props.ingredient.ingredient_name} </Text>
+                    }
+                    {this.state.inputVisible && <TextInput
+                        placeholder="New name"
+                        onChangeText={(newName) => this.setState({newName})}
+                        value={this.state.newName}
+                    />}
                     <Icon
                         name="edit"
                         size={20}
                         color="#698D3F"
-                        onPress={this.edit}
+                        onPress={this.changeName}
                         style={{ marginTop:'4%'}}
                     />
                 </View>
